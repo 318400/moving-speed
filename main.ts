7ee1c85f5640e4609eaedc45c605ba19791b1cda
318400 <91246976+318400@users.noreply.github.com>
2021-12-16 16:46:28 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,115 @@
+function led2 (y: number, speed: number) {
+    if (A == 1) {
+        while (true) {
+            for (let index = 0; index <= 4; index++) {
+                if (list[y][index] == 1) {
+                    led.plot(y, index)
+                } else {
+                    led.unplot(y, index)
+                }
+            }
+            list[y].unshift(list[y].pop())
+            basic.pause(speed)
+        }
+    }
+}
+function led22 (y: number, speed: number) {
+    if (A == 2) {
+        while (true) {
+            for (let index = 0; index <= 4; index++) {
+                if (list[y][index] == 1) {
+                    led.plot(index, y)
+                } else {
+                    led.unplot(index, y)
+                }
+            }
+            list[y].unshift(list[y].pop())
+            basic.pause(speed)
+        }
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    A = 1
+})
+input.onButtonPressed(Button.B, function () {
+    A = 2
+})
+let A = 0
+let list: number[][] = []
+list = [
+[
+1,
+0,
+0,
+0,
+0
+],
+[
+1,
+0,
+0,
+0,
+0
+],
+[
+1,
+0,
+0,
+0,
+0
+],
+[
+1,
+0,
+0,
+0,
+0
+],
+[
+1,
+0,
+0,
+0,
+0
+],
+[
+0,
+0,
+0,
+0,
+0
+]
+]
+while (false) {
+    A = 0
+}
+control.inBackground(function () {
+    led2(0, 1000)
+})
+control.inBackground(function () {
+    led22(0, 1000)
+})
+control.inBackground(function () {
+    led22(1, 500)
+})
+control.inBackground(function () {
+    led2(1, 500)
+})
+control.inBackground(function () {
+    led2(2, 250)
+})
+control.inBackground(function () {
+    led22(2, 250)
+})
+control.inBackground(function () {
+    led2(3, 200)
+})
+control.inBackground(function () {
+    led22(3, 200)
+})
+control.inBackground(function () {
+    led2(4, 100)
+})
+control.inBackground(function () {
+    led22(4, 100)
+})
